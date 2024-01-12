1b000de93292a0f381164402f68310e28b266307
2018180024 <bmbc98@naver.com>
2024-01-12 16:03:48 +0900

    InflearnServer
    인프런서버 환경설정

---
+++ Server/Server/ServerCore/Types.h
@@ -0,0 +1,14 @@
+#pragma once
+// 매크로 형식으로 치환해서 사용
+
+// __가 귀찮아서 ... 편리하게 이렇게 
+
+using BYTE = unsigned char;
+using int8 = __int8;
+using int16 = __int16;
+using int32 = __int32;
+using int64 = __int64;
+using uint8 = unsigned __int8;
+using uint16 = unsigned __int16;
+using uint32 = unsigned __int32;
+using uint64 = unsigned __int64;
