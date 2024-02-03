12a4c997df3a7a1a71d7494c3e517f7bf4a3f732
Shin Dong Yeob <bmbc98@naver.com>
2024-02-03 23:44:19 +0900

    commit

--- Server/Server/ServerCore/CoreGlobal.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-// 전역으로 사용할 매니저류의 클래스 들을 관리 
-
-extern class ThreadManager* GThreadManager;
-
-
-
--- Server/Server/ServerCore/CoreTLS.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-//표준에도 TheradID를 추출하는 기능이 있지만? 
-//1,2,,, 순서가 아닌 중부난방이기 때문에 직접 관리하자 
-
-extern thread_local uint32 LThreadId;
-
