89085f5c15d87161a396de1dbabee4c7d5c0484a
ffasdd <nhc0830@naver.com>
2024-03-25 23:38:56 +0900

    Revert "실행은 되는중입니당"
    This reverts commit f6305dd105c574ccfa2f64bdd95a6f98bbee389f.

--- animation/Scene.h
+++ animation/Scene.h
@@ -86,8 +86,6 @@ public:
 
 	bool ProcessInput(UCHAR *pKeysBuffer);
     void AnimateObjects(float fTimeElapsed);
-
-	void OnPrepareRender(ID3D12GraphicsCommandList* pd3dCommandList, CCamera* pCamera);
     void Render(ID3D12GraphicsCommandList *pd3dCommandList, CCamera *pCamera=NULL);
 
 	void ReleaseUploadBuffers();
