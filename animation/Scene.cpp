ce3121b83f518d5747ff4595301e500752cbeb26
ffasdd <nhc0830@naver.com>
2024-07-28 14:30:06 +0900

    플레이어 체력 관련 구현

--- animation/Scene.cpp
+++ animation/Scene.cpp
@@ -589,6 +589,10 @@ void CScene::AnimateObjects(float fTimeElapsed)
 			m_ppHierarchicalGameObjects[i]->m_pSkinnedAnimationController->m_bIsDead = true;
 	}*/
 
+	if (m_pPlayer->GetHealth() < 0) {
+		m_pPlayer->SetHealth(0);
+		m_pPlayer->m_pSkinnedAnimationController->m_bIsDead = true;
+	}
 	// monster dead
 	for (int i = 0; i < m_nHierarchicalGameObjects; i++)
 	{
